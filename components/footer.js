b9ad70932bcdf650acd885f50d1ae2508686fb4f
Qing Guo <kevin.guoqing@gmail.com>
2020-04-20 20:52:15 -0700

    remove personal info

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,6 @@ import styles from "./footer.module.css";
 export default function Footer(props) {
   return (
     <footer className={styles.footer}>
-      <div>Copyright &copy; Kevin Guo</div>
       <div>Powered by FourSquare API</div>
       <div>Powered by Next.js framework</div>
     </footer>
